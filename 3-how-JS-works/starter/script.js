adb41852c89668054895c0cdae2da42113d07e0d
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2019-12-23 09:25:01 -0600

    Hoisting

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,18 +1,29 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//Functions
+calculateAge(1965);
+
+function  calculateAge(year){
+    console.log(2019 - year);
+}
 
 
+//retirement(1956); wont work -> var hasn´t been created yet
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
 
+//Variables
+console.log(age);
+var age = 23;
 
-
-
-
-
-
-
-
-
+function foo(){
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
