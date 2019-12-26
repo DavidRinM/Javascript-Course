a6c1de96b818b3d6b90b2ec499309b9d2df596b2
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2019-12-26 11:47:26 -0600

    Primitives vs Objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -49,3 +49,42 @@ var jane = Object.create(personProto,
         yearofBirth: { value: 1969},
         job: { value: "Designer"}
     });
+
+
+
+// ******** Primitives vs Objects
+
+//      Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a,b); // Values are different
+
+
+//      Objects       
+var obj1 = {
+    name: "John",
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);  //Still are the same value
+
+//      Functions
+var age = 27;
+var obj = {
+    name: "Jonas",
+    city: "Lisbon"
+};
+
+function change(a, b){
+    a = 30;
+    b.city = "San Francisco";
+}
+
+change(age, obj);
+
+console.log(age); //Primitive values stays as 27
+console.log(obj.city); //Object value changes
