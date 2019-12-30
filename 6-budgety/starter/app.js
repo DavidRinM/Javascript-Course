a33ca39915789047059658f5d476e4b343a791aa
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2019-12-30 13:04:20 -0600

    Read Input Data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -43,6 +43,28 @@ var budgetController = (function (){ /* Module Pattern using  IIFE´s*/
 
 //UI CONTROLLER
 var uiController = (function(){
+
+    var DOMstrings = {
+        inputType : ".add__type",
+        inputDescription : ".add__description",
+        inputValue : ".add__value",
+        inputButton : ".add__btn"
+    };
+
+    //Function to read input data
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
 
 // Modules above are independent from each other
@@ -51,18 +73,19 @@ var uiController = (function(){
 // GOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, uiCtrl){ //We can pass arguments to modules}}
     
+    var DOM = uiCtrl.getDOMstrings();
     var ctrlAddItem = function(){
-        /*
-        1.-Get the filled input data
-        2.-Add the item to the budget controller
-        3.-Add the item to the UI
-        4.-Calculate the budget
-        5.-Display the budget on the UI
-        */
-       console.log("It works");
+        
+        //1.-Get the filled input data
+        var input = uiCtrl.getInput();
+        console.log(input);
+        //2.-Add the item to the budget controller
+        //3.-Add the item to the UI
+        //4.-Calculate the budget
+        //5.-Display the budget on the UI
     };
 
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener("click", ctrlAddItem);
     document.addEventListener("keypress", function(event){ //We want to enable the user tu use the enter button
         if(event.keyCode === 13 || event.which === 13){ //We specify the enter keycode
             ctrlAddItem();
