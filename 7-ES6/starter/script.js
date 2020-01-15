03bdff7ab6cb57cabd84d1d72ac45d36202a40de
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2020-01-14 20:43:33 -0600

    Arrow Functions 2: this keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -92,7 +92,7 @@ console.log(n.endsWith('h'));
 console.log(n.includes(' '));
 
 console.log(`${firstName} `.repeat(5));
-*/
+
 
 
 // Arrow Functions
@@ -119,3 +119,61 @@ ages6 = years.map((element,index) => {
     return `Age element ${index + 1}: ${age}`;
 });
 console.log(ages6);
+*/
+
+
+// Arrow Function 2
+
+// ES5 code
+var box5 = {
+    color: "green",
+    position: 1,
+    clickMe: function(){
+        var self = this;
+        document.querySelector(".green").addEventListener("click", function(){
+            var str = "This is box number " + self.position + " and it is " + self.color;
+            alert(str);
+        });
+    }
+};
+
+//box5.clickMe();
+
+// ES6 code
+const box6 = {
+    color: "green",
+    position: 1,
+    clickMe: function(){
+        document.querySelector(".green").addEventListener("click", () => {
+            const str = "This is box number " + this.position + " and it is " + this.color;
+            alert(str);
+        });
+    }
+};
+
+//box6.clickMe();
+
+function Person(name){
+    this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = function(friends){
+    var arr = friends.map(function(element){
+        return this.name + " is friend with " + element;
+    }.bind(this));
+
+    console.log(arr);
+}
+
+var friends = ["Bob", "Jane", "Mark"];
+new Person("John").myFriends5(friends);
+
+
+// ES6
+Person.prototype.myFriends6 = function(friends){
+    const arr = friends.map((element) => `${this.name} is firends with ${element}`);
+    console.log(arr);
+}
+
+new Person("Mike").myFriends6(friends);
