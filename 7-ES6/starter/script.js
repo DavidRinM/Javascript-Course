9e7d81bb015c8f9af2d5671970edb2a5defcc9e3
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2020-01-15 16:19:21 -0600

    Arrays in ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -177,7 +177,7 @@ Person.prototype.myFriends6 = function(friends){
 }
 
 new Person("Mike").myFriends6(friends);
-*/
+
 
 // Destructuring -> get data structure data into one variable
 
@@ -214,3 +214,56 @@ function calcAgeRetirement(year){
 const [age2, retirement] = calcAgeRetirement(1990);
 console.log(age2);
 console.log(retirement);
+*/
+
+// Arrays
+const boxes = document.querySelectorAll(".box"); //node list
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+
+//boxesArr5.forEach(function(current){
+//    current.style.backgroundColor = "dodgerblue";
+//});
+
+
+// ES6
+boxesArr6 = Array.from(boxes);
+Array.from(boxes).forEach(current => current.style.backgroundColor = "dodgerblue")
+
+//Looping through arrays
+
+//ES5
+for(var i = 0; i < boxesArr5.length; i++){
+    if(boxesArr5[i].className === "box blue"){
+        continue;
+    }
+
+    //boxesArr5[i].textContent = "I changed to blue";
+}
+
+
+// ES6
+for (const current of boxesArr6){
+    if (current.className.includes("blue")){
+        continue;
+    }
+    current.textContent = "I changed to blue";
+}
+
+//Find element in an array
+
+// ES5
+var ages = [12, 17, 8, 21, 14 ,11];
+var fullAge = ages.map(function(current){
+    return current >=18;
+});
+console.log(fullAge);
+console.log(fullAge.indexOf(true));
+console.log(ages[fullAge.indexOf(true)]);
+
+
+// ES6
+console.log(ages.findIndex((current) => current >=18)); //returns index when expression its true
+
+console.log(ages.find(current => current >= 18)); // returns value
