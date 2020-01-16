8cd1149d065a10d4e56cf6cad7a08c4f448a5d68
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2020-01-16 17:53:03 -0600

    SubClasses

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -423,7 +423,7 @@ const ans = parseInt(prompt("Write the correct answer"));
 
 //question.get(ans === question.get("answer"));
 console.log(question.get(ans === question.get("correct")));
-*/
+
 
 
 //  Classes
@@ -465,3 +465,74 @@ class Person6 {
 const john6 = new Person6("Jhon", 1990, "Teacher");
 
 Person6.greeting();
+*/
+
+
+// SubClasses
+
+// ES5 code
+var Person5 = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+};
+
+Person5.prototype.calculateAge = function(){
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals){
+    
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+
+}
+
+Athlete5.prototype = Object.create(Person5.prototype); //Now are connected
+
+Athlete5.prototype.wonMedal = function(){
+    this.medals++;
+    console.log(this.medals);
+}
+
+var johnAthlete5 = new Athlete5("John", 1990, "swimmer", 3, 10);
+//johnAthlete5.calculateAge();
+//johnAthlete5.wonMedal();
+
+
+
+// ES6 code
+
+class Person6 {
+    
+    constructor (name, yearOfBirth, job){
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge(){
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+
+}
+
+class Athlete6 extends Person6 {
+    constructor (name, yearOfBirth, job, olympicGames, medals){
+        super(name, yearOfBirth, job); //to use the same  values
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    wonMedal(){
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+const johnAthlete6 = new Athlete6("John", 1990, "Swimmer", 3, 10);
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
