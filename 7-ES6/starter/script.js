bcc55dcaf9b4862b67c13c779b7b719e7e0e8013
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2020-01-16 19:48:29 -0600

    Coding Challenge 8

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -465,7 +465,7 @@ class Person6 {
 const john6 = new Person6("Jhon", 1990, "Teacher");
 
 Person6.greeting();
-*/
+
 
 
 // SubClasses
@@ -536,3 +536,116 @@ class Athlete6 extends Person6 {
 const johnAthlete6 = new Athlete6("John", 1990, "Swimmer", 3, 10);
 johnAthlete6.wonMedal();
 johnAthlete6.calculateAge();
+
+
+
+
+    CODING CHALLENGE 8
+
+Supposethat you´re working in a small town administration and you´re in charge of two town elements:
+1.-Parks
+2.-Streets
+
+There´s only 3 parks and 4 streets. All parks and streets have a name and a build year
+
+At an end-of-year meeting, your boss wants a final reportwith the following:
+
+1.- Tree density of each park in town (formula = number of trees / park area)
+2.- Averag age of each town´s park (sum of ages / number of parks)
+3.- Name of the park that has more than 1000 trees
+4.- Total and average length of the town´s streets
+5.- Size classification of all streets: tiny / small / normal / big / huge
+    if size is unknown, the default is normal
+
+All the reported data should be printed to the console
+HINT: Use classes, subclasses, template strings, default parameters, maps, arrow functions, destructing
+*/
+
+class City{
+    constructor(name, buildYear){
+        this.name = name;
+        this.buildYear = buildYear;
+    }
+}
+
+class Park extends City{
+    constructor(name, buildYear, area, numTrees){
+        super(name, buildYear);
+        this.area = area;
+        this.numTrees = numTrees;
+    }
+
+    treeDensity() {
+        let density = this.numTrees / this.area;
+        console.log(`${this.name} has a tree density of ${density} trees per km`);
+    }
+}
+
+class Street extends City{
+    constructor(name, buildYear, length, size = 3){ //Default parameter = Normarl
+        super(name, buildYear);
+        this.length = length;
+        this.size = size;
+    }
+
+    classifyStreet() {
+        const classification = new Map();
+        classification.set(1, "Tiny");
+        classification.set(2, "Small");
+        classification.set(3, "Normal");
+        classification.set(4, "Big");
+        classification.set(5, "Huge");
+
+        console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street`);
+                                                                                      //Street size
+    }
+}
+
+const allParks = [new Park("Green Park", 1987, 0.2, 215), new Park("National Park", 1894, 2.9, 3541), new Park("Oak Park", 1953, 0.4, 949)];
+
+const allStreets = [new Street("Ocean Avenue", 1999, 1.1, 4), new Street("Evergreen Street", 2008, 2.7, 2), new Street("4th Street", 2015, 0.8), new Street("Sunset Boulevard", 1982, 2.5, 5)];
+
+
+function calc(arr){
+
+    //Acumulates all values in an array into one single value
+    const sum = arr.reduce((previous, current, index)=>  //previous value, current, index
+    previous + current, 0); //Position where the method starts 
+
+    return [sum, sum/arr.length];
+
+}
+
+
+function reportParks(p){
+
+    console.log("-------PARKS REPORT");
+    // Density
+    p.forEach(el => el.treeDensity());
+
+    // Average Age
+    const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+    const [totalAge, aveAge] = calc(ages);
+    console.log(`Our ${p.length} parks have an average age of ${aveAge}`);
+
+    // Which park has more than 1000 trees
+    const index = p.map(el => el.numTrees).findIndex(el => el>=1000);
+                    // Returns an array
+    console.log(`${p[index].name} has more than 1000 trees`);
+
+}
+
+function reportStreets(s){
+
+    console.log("-------STREETS REPORT");
+    // Total and Average length of town´s streets
+    const [totalLength, avgLength] = calc(s.map(el => el.length));
+    console.log(`Our ${s.length} streets have a total length of ${totalLength} and an average of ${avgLength}`);
+
+    // Classify size
+    s.forEach(el => el.classifyStreet());
+
+}
+
+reportParks(allParks);
+reportStreets(allStreets);
