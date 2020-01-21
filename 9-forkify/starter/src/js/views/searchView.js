f1773b3ab47f5e3705c5356ced02aa9b73ff110d
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2020-01-21 15:11:34 -0600

    Search View Part 2

--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -12,6 +12,31 @@ export const clearResults = () => {
     elements.searchResultList.innerHTML = "";
 };
 
+
+// "Pasta with Tomato and Spinach"
+/*
+acc: 0 / acc + current.length = 5 / newtitle = ["Pasta"]
+acc: 5 / acc + current.length = 9 / newtitle = ["Pasta", "with"]
+acc: 9 / acc + current.length = 15 / newtitle = ["Pasta", "with", "Tomato"]
+acc: 15 / acc + current.length = 18 / newtitle = ["Pasta", "with", "Tomato"]
+acc: 18 / acc + current.length = 24 / newtitle = ["Pasta", "with", "Tomato"]
+*/
+const limitRecipeTitle = (title, limit = 17) =>{
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(" ").reduce((accumulator, current) => {
+            if(accumulator + current.length <= limit){
+                newTitle.push(current);
+            }
+            return accumulator + current.length;
+        }, 0); //Accumulator starts at 0
+
+        //return the result
+        return `${newTitle.join(" ")} ...`;
+    }
+    return title
+}
+
 const renderRecipe= recipe => {
     const markup = `
         <li>
@@ -20,7 +45,7 @@ const renderRecipe= recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
