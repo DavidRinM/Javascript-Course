ca46931909b3dbc756fc0dde203941566b03b0e7
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2020-01-21 13:09:55 -0600

    First API calls

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,7 +1,23 @@
 //GLOBAL APP CONTROLLER
-import str from "./models/Search";
+// import str from "./models/Search";
 
-//import {add as a, multiply as m, ID} from "./views/searchView";
-import * as searchView from "./views/searchView"; //Import everything
+// //import {add as a, multiply as m, ID} from "./views/searchView";
+// import * as searchView from "./views/searchView"; //Import everything
 
-console.log(`Ùsing imported functions ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
+// console.log(`Ùsing imported functions ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
+import axios from "axios";
+
+
+async function getResult(query){ //No API key in this case nor proxy
+    try{
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    }
+    catch(error){
+        alert(error);
+    }
+
+}
+
+getResult("Pasta");
