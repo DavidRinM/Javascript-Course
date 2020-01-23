db784fc6a89e1a9fe98fd89b42e54a178a0b1e09
DavidRinM <57052947+DavidRinM@users.noreply.github.com>
2020-01-22 18:22:12 -0600

    Update Recipe Servings

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -102,3 +102,21 @@ const controlRecipe = async () => {
 // window.addEventListener("hashchange", controlRecipe);
 // window.addEventListener("load", controlRecipe);
 ["hashchange", "load"].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+// Handling Recipe Buttons
+elements.recipe.addEventListener("click", e => {
+    if(e.target.matches(".btn-decrease, .btn-decrease *")) {
+        // Decrease button in slicked
+        if(state.recipe.servings > 1){
+            state.recipe.updateServings("dec");
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    }
+    else if(e.target.matches(".btn-increase, .btn-increase *")) {
+        // Decrease button in slicked
+        state.recipe.updateServings("inc");
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+    console.log(state.recipe);
+});
