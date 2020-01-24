7cae11b96ee860495d5fa03a92e263458318783d
David Rincon <david.rincon.morales@outlook.com>
2020-01-23 21:33:40 -0600

    Likes Model

---
+++ 9-forkify/starter/src/js/models/Likes.js
@@ -0,0 +1,27 @@
+export default class Likes {
+    constructor (){
+        this.likes = [];
+    }
+
+    addLike (id, title, author, img){ //Things we are displaying
+        const like = {id, title, author, img};
+
+        this.likes.push(like);
+        return like
+    }
+
+    deleteLike (id){
+        const index = this.likes.findIndex(el => el.id === id);
+
+        this.likes.splice(index, 1); 
+    }
+
+    isLiked (id){ //To know if its a liked recipe and display it
+        return this.likes.findIndex(el => el.id === id) !== -1;
+        //return if its diff to -1 (liked recipe)
+    }
+
+    getNumberLikes (){
+        return this.likes.length;
+    }
+}
