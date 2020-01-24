0f0eb07685bd8dbf8f4d6d79ed14e48d1d7ef177
David Rincon <david.rincon.morales@outlook.com>
2020-01-23 21:13:06 -0600

    Shopping List Controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -9,6 +9,7 @@ import Recipe from "./models/Recipe"
 import List from "./models/List"
 import * as searchView from "./views/searchView"
 import * as recipeView from "./views/recipeView"
+import * as listView from "./views/listView"
 import {elements, renderLoader, clearLoader} from "./views/base"
 /** Global State of the App
  * -Search Object
@@ -17,6 +18,7 @@ import {elements, renderLoader, clearLoader} from "./views/base"
  * -Liked Recipes
 */
 const state = {};
+window.state = state;
 
 // ------- SEARCH CONTROLLER
 const controlSearch = async () => {
@@ -119,7 +121,39 @@ elements.recipe.addEventListener("click", e => {
         state.recipe.updateServings("inc");
         recipeView.updateServingsIngredients(state.recipe);
     }
+    else if(e.target.matches(".recipe__btn--add, .recipe__btn--add *")){
+        controlList();
+    }
     console.log(state.recipe);
 });
 
-window.l = new List();
+// LIST CONTROLLER
+const controlList = () => {
+    // Create a new List IF there`s none yet
+    if(!state.List) state.list = new List();
+
+    //Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient); //Get the item from model
+        listView.renderItem(item); //Render in the view
+    });
+}
+
+//Handle delete and update List Item events
+elements.shopping.addEventListener("click", e => {
+    const id = e.target.closest(".shopping__item").dataset.itemid;
+
+    //Handle delete button
+    if(e.target.matches(".shopping__delete, .shopping__delete *")){
+        //Delete from state
+        state.list.deleteItem(id);
+
+        // Delete from UI
+        listView.deleteItem(id);
+
+    }//Handle Count update
+    else if(e.target.matches(".shopping__count-value")){
+        const val =parseFloat(e.target.value,10)
+        state.list.updateCount(id, val);
+    }
+});
